f55a47a6b0d827c7c8418926745f8db13a9e4c29
DanielDob <35843305+DanielDob@users.noreply.github.com>
2018-02-05 00:12:58 +0100

    Main menu

--- app/src/main/java/com/example/daniel/gymassistant/MainActivity.java
+++ app/src/main/java/com/example/daniel/gymassistant/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.daniel.gymassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout parent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        parent = (LinearLayout) View.inflate(this,R.layout.activity_main,null);
+        setContentView(parent);
+    }
+
+    public void startTraining(View view) {
+    }
+
+    public void addTraining(View view) {
+    }
+
+    public void progressOpen(View view) {
+    }
+
+    public void timetableOpen(View view) {
+    }
+
+    public void synchOpen(View view) {
+    }
+
+    public void optionsOpen(View view) {
     }
 }
