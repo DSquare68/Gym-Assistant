0c7106b6ce0ffee403a10089599cbb69970f9564
DanielDob <35843305+DanielDob@users.noreply.github.com>
2020-03-05 15:58:14 +0100

    new start
    Start code app from beginning

--- app/src/main/java/com/example/daniel/gymassistant/MainActivity.java
+++ app/src/main/java/com/example/daniel/gymassistant/MainActivity.java
@@ -7,12 +7,6 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.example.daniel.database.exercise.name.DatebaseOfexerciseNames;
-import com.example.daniel.database.exercise.name.Exercise;
-import com.example.daniel.database.exercise.name.ExerciseDatabase;
-import com.example.daniel.values.Resolution;
-import com.example.daniel.values.SettingsValues;
-
 import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,46 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         parent = (LinearLayout) View.inflate(this,R.layout.activity_main,null);
         setContentView(parent);
-        if(SettingsValues.getValue(SettingsValues.FIRST_OPEN_APP,getApplicationContext())==1){
-            Toast toast = Toast.makeText(getApplicationContext(),getResources().getString(R.string.welcome),Toast.LENGTH_LONG);
-            toast.show();
-            SettingsValues.setValue(SettingsValues.FIRST_OPEN_APP,getApplicationContext(),-1);
-            for(int i = 0; i< DatebaseOfexerciseNames.nazwyTreningów.length; i++){
-                ExerciseDatabase trainingName = new ExerciseDatabase(getApplicationContext());
-                trainingName.addExercise(new Exercise(DatebaseOfexerciseNames.nazwyTreningów[i]));
-            }
-        }
-        new Resolution(this);
-    }
 
-    public void startTraining(View view) {
-        Intent intent = new Intent(this, StartTraining.class);
-        startActivity(intent);
-
-    }
-
-    public void addTraining(View view) {
-        Intent intent = new Intent(this, AddTraining.class);
-        startActivity(intent);
-    }
-
-    public void progressOpen(View view) {
-        Intent intent = new Intent(this, Statistics.class);
-        startActivity(intent);
-    }
-
-    public void timetableOpen(View view) {
-        Intent intent = new Intent(this, Timetable.class);
-        startActivity(intent);
-    }
-
-    public void synchOpen(View view) {
-        Intent intent = new Intent(this, Synch.class);
-        startActivity(intent);
-    }
-
-    public void optionsOpen(View view) {
-        Intent intent = new Intent(this, Settings.class);
-        startActivity(intent);
     }
 }
