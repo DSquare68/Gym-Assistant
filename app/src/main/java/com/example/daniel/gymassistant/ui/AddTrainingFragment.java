50f873fdcd343ddf09baba7cd07917abe5a1b5b5
DanielDob <35843305+DanielDob@users.noreply.github.com>
2020-04-10 20:09:21 +0200

    spelling

--- app/src/main/java/com/example/daniel/gymassistant/ui/AddTrainingFragment.java
+++ app/src/main/java/com/example/daniel/gymassistant/ui/AddTrainingFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.daniel.gymassistant.R;
 
-public class AddTreningFragment extends Fragment {
+public class AddTrainingFragment extends Fragment {
 
     @Nullable
     @Override
